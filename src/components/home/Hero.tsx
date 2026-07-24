20c90994d202522121f70854cfd0153eb2f66fd7
Josue Daniel <josuel.barreras@gmail.com>
2026-07-24 10:52:24 -0700

    chore: add playsinline=1 to youtube embed to prevent iOS native controls

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -43,7 +43,7 @@ export default function Hero({ locale }: { locale: string }) {
           className="absolute inset-0 w-full h-full scale-[1.15]"
         >
           <iframe
-            src="https://www.youtube-nocookie.com/embed/vlwHFitriQ4?autoplay=1&mute=1&loop=1&playlist=vlwHFitriQ4&controls=0&showinfo=0&rel=0&modestbranding=1&disablekb=1&start=374&vq=hd1080&hd=1"
+            src="https://www.youtube-nocookie.com/embed/vlwHFitriQ4?autoplay=1&mute=1&loop=1&playlist=vlwHFitriQ4&controls=0&showinfo=0&rel=0&modestbranding=1&disablekb=1&playsinline=1&start=374&vq=hd1080&hd=1"
             className="absolute top-1/2 left-1/2 w-[100vw] h-[56.25vw] min-h-[100vh] min-w-[177.77vh] -translate-x-1/2 -translate-y-1/2 pointer-events-none"
             allow="autoplay; encrypted-media; picture-in-picture"
             frameBorder="0"
